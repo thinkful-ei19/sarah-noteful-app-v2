01c3b49fc62a59c8d98f195787b0dc0c0bd42821
ssheaks <sarah.sheaks@gmail.com>
2018-03-14 16:11:21 -0700

    update notes.router get notes to include folder data

--- routes/notes.router.js
+++ routes/notes.router.js
@@ -17,15 +17,23 @@ const notes = simDB.initialize(data);
 /* ========== GET/READ ALL NOTES ========== */
 router.get('/notes', (req, res, next) => {
   const { searchTerm } = req.query;
-  knex.select('id', 'title', 'content')
+  const {folderId} = req.query;
+  knex.select('notes.id', 'title', 'content', 'folders.id as folder_Id', 'folders.name as folderName')
     .from('notes')
+    .leftJoin('folders', 'notes.folder_id', 'folders.id')
     .where(function() {
       if(searchTerm) {
         this.where('title', 'like', `%${searchTerm}%`);
       }
     })
-    .then(list => {
-      res.json(list);
+    .modify(function (queryBuilder) {
+      if (folderId) {
+        queryBuilder.where('folder_Id', folderId);
+      }
+    })
+    .orderBy('notes.id')
+    .then(results => {
+      res.json(results);
     })
     .catch(err => next(err));
   console.log(res.body);
